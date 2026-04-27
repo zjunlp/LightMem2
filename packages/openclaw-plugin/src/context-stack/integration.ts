a3301784a779b2453e7c89d57f9804bd4bac3a97
Xubqpanda <1227972384@qq.com>
2026-04-27 15:08:22 +0800

    refactor: move proxy upstream wiring into integration stack

--- packages/openclaw-plugin/src/context-stack/integration.ts
+++ packages/openclaw-plugin/src/context-stack/integration.ts
@@ -4,5 +4,5 @@ export * from "./integration/runtime-helpers.js";
 export * from "./integration/runtime-register.js";
 export * from "./integration/proxy-runtime.js";
 export * from "./integration/proxy-provider.js";
-export * from "../proxy/upstream.js";
+export * from "./integration/upstream.js";
 export * from "./integration/trace-hooks.js";
